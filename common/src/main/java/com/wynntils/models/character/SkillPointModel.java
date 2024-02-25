c7f5d75fe13afa8a8e1cd6b9cccb8baadf78c5c5
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2024-02-25 02:52:59 -0800

    faster scrolling, del to delete loadouts

--- common/src/main/java/com/wynntils/models/character/SkillPointModel.java
+++ common/src/main/java/com/wynntils/models/character/SkillPointModel.java
@@ -138,6 +138,7 @@ public class SkillPointModel extends Model {
     }
 
     public void loadLoadout(String name) {
+        if (name == null) return; // no loadout selected
         ContainerUtils.closeBackgroundContainer();
 
         ScriptedContainerQuery query = ScriptedContainerQuery.builder("Loading Skill Point Loadout Query")
